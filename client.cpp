87ffd2c276307b4e59bef326d23a539d12386013
Joshua Kim <joshuakim135@gmail.com>
2021-10-21 23:57:21 -0400

    batch bugs resolved

--- client.cpp
+++ client.cpp
@@ -1,37 +1,51 @@
 #include "common.h"
 #include "FIFOreqchannel.h"
 #include <sys/wait.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 
 int main(int argc, char *argv[]){
 	int opt;
 	int p = 1;
-	double t = -0.1;
+	double t = -1;
 	int e = -1;
 	string filename = "";
 	int buffercapacity = MAX_MESSAGE;
 	string bcapstring = "";
 	bool isNewChan = false;
+	bool reqFile = false;
 	// take all the arguments first because some of these may go to the server
+	// p->patient #, t->time (0-60), e->ecg val (1 or 2), f->
 	while ((opt = getopt(argc, argv, "p:t:e:f:m:")) != -1) {
 		switch (opt) {
 			case 'f':
 				filename = optarg;
+				reqFile = true;
+				std::cout << "File name: " << filename << endl;
 				break;
 			case 'p':
 				p = atoi (optarg);
+				std::cout << "Patient number: " << p << endl;
+				break;
 			case 't':
 				t = atof(optarg);
+				std::cout << "time: " << t << endl;
 				break;
 			case 'e':
 				e = atoi (optarg);
+				std::cout << "ecg number: " << e << endl;
 				break;
 			case 'm':
 				bcapstring = optarg;
 				buffercapacity = atoi (optarg);
+				std::cout << "buffer cap: " << buffercapacity << endl;
 				break;
 			case 'c':
 				isNewChan = true;
+				std::cout << "Requesting New Channel" << endl;
 				break;
 		}
 	}
@@ -41,55 +55,80 @@ int main(int argc, char *argv[]){
 		EXITONERROR ("Could not create a child process for running the server");
 	}
 	if (!pid){ // The server runs in the child process
-		char* args[] = {"./server", "-m", (char*)bcapstring.c_str(), nullptr};
+		char* args[] = {"./server", "-m", (char*)to_string(buffercapacity).c_str(), nullptr};
 		if (execvp(args[0], args) < 0){
 			EXITONERROR ("Could not launch the server");
 		}
 	}
+
 	FIFORequestChannel chan ("control", FIFORequestChannel::CLIENT_SIDE);
-	if (t == -.1 && e == -1) {
+	if (t == -1 && e == -1) {
+		string contents;
+		ostringstream ostr;
 		for (int i = 0; i < 1000; i++) {
+			double time = i*0.004;
 			DataRequest d(p, i*0.004, 1);
 			chan.cwrite (&d, sizeof (DataRequest)); // question
 			double reply;
 			chan.cread (&reply, sizeof(double)); //answer
 			if (!isValidResponse(&reply)){
+				std::cout << "Invalid Response 1" << endl;
 				exit (0);
-				// cout << "For person " << p <<", at time " << t << ", the value of ecg "<< e <<" is " << reply << endl;
 			}
 
+			DataRequest d2(p, i*0.004, 2);
+			chan.cwrite (&d2, sizeof (DataRequest));
+			double reply2;
+			chan.cread(&reply2, sizeof(double));
+			if (!isValidResponse(&reply2)) {
+				std::cout << "Invalid Response 2" << endl;
+				exit(0);
+			}
+
+			// print out output for testing
+			std::cout << "time: " << time << " -> " << reply << " " << reply2 << endl;
+			
+			// write to file
+			ostr << time << "," << reply << "," << reply2 << endl;
 		}
+		contents = ostr.str();
+		fstream file;
+		string fname = to_string(p) + "_first_1000.csv";
+		file.open(fname, fstream::out);
+		file << contents;
 	}
 
 	/* this section shows how to get the length of a file
 	you have to obtain the entire file over multiple requests 
 	(i.e., due to buffer space limitation) and assemble it
 	such that it is identical to the original*/
-	DataRequest d (p, t, e);
-	FileRequest fm (0,0);
-	int len = sizeof (FileRequest) + filename.size()+1;
-	char buf2 [len];
-	memcpy (buf2, &fm, sizeof (FileRequest));
-	strcpy (buf2 + sizeof (FileRequest), filename.c_str());
-	chan.cwrite (buf2, len);  
-	int64 filelen;
-	chan.cread (&filelen, sizeof(int64));
-	if (isValidResponse(&filelen)){
-		cout << "File length is: " << filelen << " bytes" << endl;
-	}
+	if (reqFile) {
+		FileRequest fm (0,0);
+		int len = sizeof (FileRequest) + filename.size()+1;
+		char buf2 [len];
+		memcpy (buf2, &fm, sizeof (FileRequest));
+		strcpy (buf2 + sizeof (FileRequest), filename.c_str());
+		chan.cwrite (buf2, len);  
+		int64 filelen;
+		chan.cread (&filelen, sizeof(int64));
+		if (isValidResponse(&filelen)){
+			std::cout << "File length is: " << filelen << " bytes" << endl;
+		}
 	
-	int64 rem = filelen;
-	FileRequest* f = (FileRequest*) buf2;
-	ofstream of (filename);
-	char recvBuf [buffercapacity];
-	while (rem > 0) {
-		f->length = min(rem, (int64)buffercapacity);
-		chan.cwrite (buf2, sizeof (buf2));
-		chan.cread(recvBuf, buffercapacity);
-		of.write(recvBuf, f->length);
-		rem -= 	f->length;
+		// original if statemnt content
+		int64 rem = filelen;
+		FileRequest* f = (FileRequest*) buf2;
+		ofstream of (filename);
+		char recvBuf [buffercapacity];
+		while (rem > 0) {
+			f->length = min(rem, (int64)buffercapacity);
+			chan.cwrite (buf2, sizeof (buf2));
+			chan.cread(recvBuf, buffercapacity);
+			of.write(recvBuf, f->length);
+			rem -= 	f->length;
+		}
 	}
-	
+
 	if (isNewChan) {
 		Request nc (NEWCHAN_REQ_TYPE);
 		chan.cwrite(&nc, sizeof(nc));
@@ -101,10 +140,12 @@ int main(int argc, char *argv[]){
 		Request q (QUIT_REQ_TYPE);
 		newchan.cwrite (&q, sizeof(Request));
 	}
+
 	// closing the channel    
     Request q (QUIT_REQ_TYPE);
     chan.cwrite (&q, sizeof (Request));
+
 	// client waiting for the server process, which is the child, to terminate
 	wait(0);
-	cout << "Client process exited" << endl;
+	std::cout << "Client process exited" << endl;
 }
